be3f51d0e3fe5f1babe152a85131141e5851e0ae
Surajdusane <138127406+Surajdusane@users.noreply.github.com>
2025-09-20 13:36:08 +0530

    new

--- run_payslip.py
+++
@@ -1,3 +0,0 @@
-from modules.payslip import create_payslip
-
-create_payslip("pre", 11, "2022-01-10", bank_no= "38943385226", pan_no="HMVPM3987G", name="Kirti Mishra", employee_id="PS100036/57", designation="US IT RECRUITER", salary=29300)
